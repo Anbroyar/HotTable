27f12c42f0a7ef7b3294e76d50160bbe7584fe45
Ashlee Brolly <ashleebrolly@Ashlees-MacBook-Pro.local>
2018-01-25 19:24:50 -0500

    added html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+// Routes
+// =============================================================
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
