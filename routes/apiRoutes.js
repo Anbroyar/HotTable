d383d80c2f69931268921ba50ee7f9dd0e20d825
Ashlee Brolly <ashleebrolly@Ashlees-MacBook-Pro.local>
2018-01-25 19:42:08 -0500

    created api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,53 @@
+// Routes
+// =============================================================
+
+// Search for Specific Character (or all characters) - provides JSON
+app.get("/api/:reservations", function (req, res) {
+    var chosen = req.params.characters;
+    if (chosen) {
+        console.log(chosen);
+        for (var i = 0; i < characters.length; i++) {
+            if (chosen === characters[i].routeName) {
+                return res.json(characters[i]);
+            }
+        }
+        return res.json(false);
+    }
+    return res.json(characters);
+});
+
+app.get("/api/:waitlist", function (req, res) {
+    var chosen = req.params.characters;
+    if (chosen) {
+        console.log(chosen);
+        for (var i = 0; i < characters.length; i++) {
+            if (chosen === characters[i].routeName) {
+                return res.json(characters[i]);
+            }
+        }
+        return res.json(false);
+    }
+    return res.json(characters);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/waitlist", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body-parser middleware
+    var newcharacter = req.body;
+    newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newcharacter);
+    characters.push(newcharacter);
+    res.json(newcharacter);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/tables", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body-parser middleware
+    var newcharacter = req.body;
+    newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newcharacter);
+    characters.push(newcharacter);
+    res.json(newcharacter);
+});
